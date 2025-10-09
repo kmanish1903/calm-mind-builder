d8b37fd926c6179bbd68bb1ebe24354b68325cfd
KANYABOINA MANISH <123622340+kmanish1903@users.noreply.github.com>
2025-10-09 19:02:27 +0530

    Update vite.config.ts
    back to intial configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => (
-  {
+export default defineConfig(() => ({
   server: {
-  host: "0.0.0.0",
-  port: 5000,
-  allowedHosts: 'all',  // Add this line
-  hmr: {
-    clientPort: 443,
+    host: "::",
+    port: 8080,
   },
-
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      react: path.resolve(__dirname, "node_modules/react"),
+      "react": path.resolve(__dirname, "node_modules/react"),
       "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
     },
     dedupe: ["react", "react-dom"],
