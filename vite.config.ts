b0e0338d2256d5de089d3ff07fc363b010875a93
KANYABOINA MANISH <123622340+kmanish1903@users.noreply.github.com>
2025-10-09 18:49:19 +0530

    Update vite.config.ts
    allowed replit host

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,15 @@ export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "f421d72a-3aa7-4c64-ae81-0b3eb3640930-00-353blimdzsc9y.riker.replit.dev"
+    ],
   },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "react": path.resolve(__dirname, "node_modules/react"),
+      react: path.resolve(__dirname, "node_modules/react"),
       "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
     },
     dedupe: ["react", "react-dom"],
